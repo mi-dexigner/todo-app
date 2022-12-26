7ae0bf3d93d2810e8e184e8dddae205d412a879e
David <david.toledo@seoprohub.com>
2022-12-26 20:01:08 +0500

    add guides functions

--- pages/guides.js
+++ pages/guides.js
@@ -1,14 +1,22 @@
-import { useEffect } from 'react'
+import { useContext,useEffect } from 'react'
 import Layout from '../components/Layout'
 import styles from '../styles/Guides.module.css'
-
+import AuthContext from '../stores/authContext'
 export default function Guides() {
+const { user, authReady } = useContext(AuthContext)
 
-  useEffect(() => {
-    fetch('/.netlify/functions/supermario')
-      .then(res => res.json())
-      .then(data => console.log(data))
-  },[])
+    useEffect(() => {
+      if (authReady) {
+        fetch('/.netlify/functions/guides', user && {
+          headers: {
+            Authorization:  'Bearer ' + user.token.access_token
+          }
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+      }
+  
+    },[user, authReady])
 
   return (
     <Layout>
